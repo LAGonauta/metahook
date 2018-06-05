43397f9a0e44c3ebb747a72ac84c7b8061c91765
LAGonauta <lagonauta@gmail.com>
2018-06-05 17:53:31 -0300

    Changes to MetaHook ported from old MetaAudio branch

--- common/interface.cpp
+++ common/interface.cpp
@@ -1,6 +1,6 @@
 //========= Copyright © 1996-2002, Valve LLC, All rights reserved. ============
 //
-// Purpose:
+// Purpose: 
 //
 // $NoKeywords: $
 //=============================================================================
@@ -11,13 +11,16 @@
 
 #ifndef _WIN32  // LINUX
 #include <dlfcn.h>
-#include <filesystem>
+#include <unistd.h> // getcwd
+#include <stdio.h> // sprintf
 #endif
+
 
 // ------------------------------------------------------------------------------------ //
 // InterfaceReg.
 // ------------------------------------------------------------------------------------ //
 InterfaceReg *InterfaceReg::s_pInterfaceRegs = NULL;
+
 
 InterfaceReg::InterfaceReg(InstantiateInterfaceFn fn, const char *pName) :
   m_pName(pName)
@@ -26,6 +29,8 @@ InterfaceReg::InterfaceReg(InstantiateInterfaceFn fn, const char *pName) :
   m_pNext = s_pInterfaceRegs;
   s_pInterfaceRegs = this;
 }
+
+
 
 // ------------------------------------------------------------------------------------ //
 // CreateInterface.
@@ -53,34 +58,60 @@ EXPORT_FUNCTION IBaseInterface *CreateInterface(const char *pName, int *pReturnC
   return NULL;
 }
 
+
 #ifdef _WIN32
 #define WIN32_LEAN_AND_MEAN
 #include "windows.h"
 #endif
 
+
+#ifdef _WIN32
 HINTERFACEMODULE Sys_LoadModule(const char *pModuleName)
 {
-#ifdef _WIN32
   return (HINTERFACEMODULE)LoadLibrary(pModuleName);
-#else  // LINUX
-  // Linux dlopen() doesn't look in the current directory for libraries.
-  // We tell it to, so people don't have to 'install' libraries as root.
-  std::string library_path = std::filesystem::current_path().string() + "/" + pModuleName;
-
-  return (HINTERFACEMODULE)dlopen(library_path.c_str(), RTLD_NOW);
-#endif
 }
 
+#else  // LINUX
+HINTERFACEMODULE Sys_LoadModule(const char *pModuleName)
+{
+  // Linux dlopen() doesn't look in the current directory for libraries.
+  // We tell it to, so people don't have to 'install' libraries as root.
+
+  char szCwd[1024];
+  char szAbsoluteLibFilename[1024];
+
+  getcwd(szCwd, sizeof(szCwd));
+  if (szCwd[strlen(szCwd) - 1] == '/')
+    szCwd[strlen(szCwd) - 1] = 0;
+
+  sprintf(szAbsoluteLibFilename, "%s/%s", szCwd, pModuleName);
+
+  return (HINTERFACEMODULE)dlopen(szAbsoluteLibFilename, RTLD_NOW);
+}
+
+#endif
+
+
+#ifdef _WIN32
 void Sys_FreeModule(HINTERFACEMODULE hModule)
 {
   if (!hModule)
     return;
-#ifdef _WIN32
+
   FreeLibrary((HMODULE)hModule);
-#else  // LINUX
-  dlclose((void *)hModule);
-#endif
 }
+
+#else  // LINUX
+void Sys_FreeModule(HINTERFACEMODULE hModule)
+{
+  if (!hModule)
+    return;
+
+  dlclose((void *)hModule);
+}
+
+#endif
+
 
 //-----------------------------------------------------------------------------
 // Purpose: returns the instance of this module
@@ -91,19 +122,29 @@ CreateInterfaceFn Sys_GetFactoryThis(void)
   return CreateInterface;
 }
 
+
 //-----------------------------------------------------------------------------
 // Purpose: returns the instance of the named module
 // Input  : *pModuleName - name of the module
 // Output : interface_instance_t - instance of that module
 //-----------------------------------------------------------------------------
 
+#ifdef _WIN32
 CreateInterfaceFn Sys_GetFactory(HINTERFACEMODULE hModule)
 {
   if (!hModule)
-    return nullptr;
-#ifdef _WIN32
+    return NULL;
+
   return (CreateInterfaceFn)GetProcAddress((HMODULE)hModule, CREATEINTERFACE_PROCNAME);
-#else  // LINUX
-  return (CreateInterfaceFn)dlsym((void *)hModule, CREATEINTERFACE_PROCNAME);
-#endif
 }
+
+#else  // LINUX
+CreateInterfaceFn Sys_GetFactory(HINTERFACEMODULE hModule)
+{
+  if (!hModule)
+    return NULL;
+
+  return (CreateInterfaceFn)dlsym((void *)hModule, CREATEINTERFACE_PROCNAME);
+}
+
+#endif
